58a065606cd5547e4a070d405d6e7b641a8f9849
Alexander Johnson <abjohnson117@gmail.com>
2023-07-18 23:26:05 -0700

    Made small changes to blur operator

--- operators.py
+++ operators.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pywt
 from scipy.ndimage import correlate, convolve
+from scipy.signal import convolve2d
 from scipy.fftpack import dct
 
 def max_function(x, num_compare):
@@ -8,6 +9,13 @@ def max_function(x, num_compare):
 
 def prox_op(x,lambd):
     return np.sign(x)*max_function(np.abs(x)-lambd,0)
+
+def wavelet_operator(orig):
+    coeffs = pywt.wavedec2(orig, wavelet="haar", level=3)
+    wav_x, _ = pywt.coeffs_to_array(coeffs)
+    wav_x = wav_x[0:m,0:m]
+    W = np.reshape(wav_x, (m**2,1))
+    return W
 
 def wavelet_operator_1d(org):
     coeffs = pywt.wavedec(org, wavelet="haar", level=2)
@@ -28,14 +36,15 @@ def fspecial(shape=(3,3),sigma=0.5):
         h /= sumh
     return h
 
-def blur_operator(org, reshape=True, shape=(9,9), sigma=1, mode="nearest"):
+def blur_operator(org, reshape=True, shape=(9,9), sigma=1, mode="reflect"):
     if reshape:
         m = int(np.sqrt(org.shape[0]))
         org = np.reshape(org, (m,m))
 
     psf = fspecial(shape,sigma)
     blurred = correlate(org, psf, mode=mode)
-    blurred += np.random.normal(0,1e-4,size=(m,m))
+    # blurred = convolve2d(org, psf, mode="same", boundary=boundary)
+    # blurred += np.random.normal(0,1e-4,size=(m,m)) #TODO: Add this separately
     blurred = blurred.T
     if reshape:
         blurred = blurred.flatten("F")
@@ -68,8 +77,8 @@ def evals_blur(psf):
 
     S = dct(dct(a1, axis=0), axis=1) / dct(dct(e1,axis=0), axis=1)
     #TODO: CHECK THIS
-    return S
+    return np.max(S), S
 
 
-def grad(x):
-    return 2*(ATA@x)
+def grad(x,b):
+    return 
